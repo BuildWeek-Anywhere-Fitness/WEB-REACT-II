a0112798373eca41383015ad0e452773d98368a7
Adrian-Garcia <agarc5@hotmail.com>
2019-09-23 17:37:43 -0500

    final commit day1/happy with what i did/rest time

--- anywhere-fitness-fe/src/components/Register.js
+++ anywhere-fitness-fe/src/components/Register.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react'
+import Loader from "react-loader-spinner";
+import { connect } from 'react-redux';
+import { register } from '../actions'
 
 export class Register extends Component {
+    state = {
+        credentials: {
+            fullname: "",
+            username: "",
+            password: ""
+        }
+    };
+
+    handleChange = e => {
+        this.setState({
+            credentials: {
+                ...this.state.credentials,
+                [e.target.name]: e.target.value
+            }
+        });
+    };
+
+
+    register = e => {
+        e.preventDefault();
+        this.props
+            .register(this.state.credentials)
+            .then(res => {
+                if (res) {
+                    this.props.history.push("./login");
+                }
+            });
+    };
+
     render() {
         return (
             <div className="register">
                 Register Page
+                <form className="register-form" onSubmit={this.register}>
+                    <input
+                        type="text"
+                        name="fullname"
+                        placeholder="Full Name"
+                        value={this.state.credentials.fullname}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.credentials.username}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.credentials.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>
+                        {this.props.registering ? (
+                            <Loader type="ThreeDots" color="#1f2a38" height="12" width="26" />
+                        ) : (
+                                "Register"
+                            )}
+                    </button>
+                </form>
             </div>
         )
     }
 }
 
-export default Register
+const mapStateToProps = state => {
+    return {
+        error: state.registerReducer.error,
+        creatingUser: state.registerReducer.creatingUser
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    { register }
+)(Register);
